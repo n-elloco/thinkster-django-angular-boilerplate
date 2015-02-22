5f58e6947a91b79d7bb560a199352ddb48cd13e2
n-elloco <en.elloco@gmail.com>
2015-02-23 00:54:31 +0600

    Create register form

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,17 @@
 angular
-  .module('thinkster', []);
+    .module('thinkster', [
+        'thinkster.config',
+        'thinkster.routes',
+        'thinkster.authentication'
+    ])
+    .run(run);
+
+run.$inject = ['$http'];
+
+angular.module('thinkster.config', []);
+angular.module('thinkster.routes', ['ngRoute']);
+
+function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'scrftoken';
+}
